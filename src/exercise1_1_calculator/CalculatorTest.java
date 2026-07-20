1a26d96ef0de4ec5d1d0f69adcc1b76b9c1d62c5
Fatma <fatma.arslantas@outlook.com>
2026-07-20 20:48:23 +0300

    Add unit tests for Calculator

---
+++ src/exercise1_1_calculator/CalculatorTest.java
@@ -0,0 +1,37 @@
+package exercise1_1_calculator;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CalculatorTest {
+
+    private Calculator calculator;
+
+    @BeforeEach
+    void setup() {
+        calculator = new Calculator();
+    }
+
+    @Test
+    void testAdd() {
+        int result = calculator.add(2, 3);
+
+        assertEquals(5, result);
+    }
+
+    @Test
+    void testDivide() {
+        int result = calculator.divide(10, 2);
+
+        assertEquals(5, result);
+    }
+
+    @Test
+    void testDivideByZeroThrows() {
+        assertThrows(
+                IllegalArgumentException.class,
+                () -> calculator.divide(10, 0)
+        );
+    }
+}
