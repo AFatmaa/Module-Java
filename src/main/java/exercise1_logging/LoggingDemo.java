684a78d498514018e93cbd3f02f46e65be5ad80b
Fatma <fatma.arslantas@outlook.com>
2026-08-15 00:04:19 +0300

    Add logging setup and demo

---
+++ src/main/java/exercise1_logging/LoggingDemo.java
@@ -0,0 +1,22 @@
+package exercise1_logging;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class LoggingDemo {
+
+    private static final Logger log = LoggerFactory.getLogger(LoggingDemo.class);
+
+    public static void main(String[] args) {
+        log.info("Application starting...");
+        log.debug("This is a debug message that you should NOT see yet.");
+        log.warn("Low disk space!");
+        log.error("Something went wrong.", new RuntimeException("Test error"));
+
+        for (int i = 1; i <= 100; i++) {
+            if (i % 10 == 0) {
+                log.debug("Processing iteration {}", i);
+            }
+        }
+    }
+}
